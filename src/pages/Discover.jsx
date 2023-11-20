550e13c445c5dbb68c4b288ba2974b58191adfbe
Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>
2023-11-20 23:31:01 +0530

    added song cards

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,10 +1,20 @@
 import { Error, Loader, SongCard} from "../components";
 import {genres} from "../assets/constants";
 import {useGetTopChartsQuery} from '../redux/services/ShazamCore'
+import { useDispatch, useSelector } from "react-redux";
+
+
 
 const Discover = () => {
-    const {} = useGetTopChartsQuery();
+    const dispatch = useDispatch();
+    const {activeSong, isPlaying} = useSelector((state) => state.player);
+    
+    const {data, isFetching, error} = useGetTopChartsQuery();
     const genreTitle = 'Pop';
+
+    if(isFetching) return <Loader title="Loading songs......"/>
+
+    if(error) return <Error />
 
     return (<div className="flex flex-col">
         <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -18,9 +28,12 @@ const Discover = () => {
         </div>
 
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-            {[1,2,3,4,5,6,7,8,9,10].map((song, i) => (
+            {data?.map((song, i) => (
               <SongCard key={song.key}
               song={song}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              data={data}
               i = {i}
               />  
             ))}
--- src/redux/services/ShazamCore.js
+++ src/redux/services/ShazamCore.js
@@ -1,23 +1,19 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-  export const shazamCoreApi = createApi({
-    reducerPath: 'shazamCoreApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-        prepareHeaders: ()=>{
-            headers.set(
-              "X-RapidAPI-Key",
-              "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA"
-            );
-            return headers;
-        }
-        }),
-        endpoints: (builder) => ({
-            getTopCharts: builder.query({query: '/charts/world'}),
-        }),
-    });
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {  
+      headers.set("X-RapidAPI-Key", "KJwZZIJSFimshuivMSVGaiYzkRomp15f2vKjsnK4bKzuUzVLzA");
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
 
-
-    export const {
-        useGetTopChartsQuery,
-    } = shazamCoreApi;
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
